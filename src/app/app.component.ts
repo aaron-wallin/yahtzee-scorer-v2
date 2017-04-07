564fca24a2ae04448168d912b5e408a146d94edd
aaron-wallin <aaron.wallin@gmail.com>
2017-04-07 12:56:25 -0700

    Improve layout and menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,41 +16,7 @@ import {AuthService} from './user/auth.service';
 
 @Component({
     selector: 'app-root',
-    template: `
-    <div class='container-fluid'>
-
-        <nav class='navbar navbar-default'>
-            <div class='container-fluid'>
-                
-                    <a class='navbar-brand'>{{pageTitle}}</a>
-                
-
-                <div class='collapse navbar-collapse'>
-                    <ul class='nav navbar-nav'>
-                        <li><a [routerLink]="['challenges']">Home</a></li>
-                        <li><a [routerLink]="['challenges/new']">New Challenge</a></li>
-                    </ul>
-
-                    <div class="navbar-header navbar-right">
-                        <ul class="nav navbar-nav">
-                          <li>
-                            <a *ngIf="!authService.isAuthenticated()" [routerLink]="['user/login']">Login</a>
-                            <div>                                
-                                <a *ngIf="authService.isAuthenticated()">{{authService.currentUser.firstName}}</a>
-                                <button *ngIf="authService.isAuthenticated()" (click)="logout()">Logout</button>
-                            </div>
-                          </li>
-                        </ul>
-                    </div>
-                </div>
-
-            </div>
-        </nav>  
-        <div class='container-fluid'>
-            <router-outlet></router-outlet>
-        </div>    
-    </div>
-    `,
+    templateUrl: './app.component.html',
     providers: [FirebaseService]
 })
 
